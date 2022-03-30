c466e2b17aa5645e1611a314f6b8fd9904360714
gopal das <gopal1409@gmail.com>
2022-03-30 21:01:20 +0530

    terraform local module

--- terraform-manifest/azure-static-website/main.tf
+++ terraform-manifest/azure-static-website/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_resource_group" "resource_group" {
 #create an storage account
 
 resource "azurerm_storage_account" "storage_account" {
-  name  = "{var.storage_account_name}${random_string.myrandom.id}"
+  name  = "${var.storage_account_name}${random_string.myrandom.id}"
   resource_group_name = azurerm_resource_group.resource_group.name
   location = var.location
   account_tier = var.storage_account_tier
